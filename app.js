4baff6cebcdf0fb4aa20be4cc04236d568f1928a
Omkar Kambire <omkar.yupgp22@sinhgad.edu>
2025-10-06 14:13:19 +0530

    bcrypt

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 
 app.use(cookieParser());
 
 app.get("/", ( req, res) => {
-    let token = jwt.sign({ email: "omkar@test.com" }, "secret");
-    res.cookie("token", token);
-    res.send("Cookie has been set");
+    bcrypt.compare("Omkar123", "$2b$10$CrEMQWNbovU9BORwWCSX7.YqS1.5rL5Mfr/U4.ehp/KO2.hjc4uUK", function(err, result) {
+    console.log(result);
+    
 });
 
-app.get("/read", (req, res ) => {
-    let data = jwt.verify(req.cookies.token, "secret");
-    console.log(data); 
-})
+res.send("Done");
+});
+
 
 app.listen(3000);
